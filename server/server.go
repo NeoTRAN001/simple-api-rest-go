c1a7dacf6d512f0669a77bb1ec973d39b61384e1
NeoTRAN <neotran@localhost.localdomain>
2022-10-10 20:10:02 -0500

    first commit

---
+++ server/server.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"platzi.com/go/rest-ws/database"
+	"platzi.com/go/rest-ws/repository"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("Port is required")
+	}
+
+	if config.JWTSecret == "" {
+		return nil, errors.New("JWTSecret is required")
+	}
+
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("DatabaseUrl is required")
+	}
+
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+
+	repo, err := database.NewPostgresRepository(b.config.DatabaseUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository.SetRepository(repo)
+
+	log.Println("Starting server on port", b.config.Port)
+
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	} else {
+		log.Fatalf("Server stoppped")
+	}
+}
