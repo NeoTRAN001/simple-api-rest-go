18637115f0b2dd9a735b8a4b858e8de1878f9f7f
NeoTRAN <neotran@localhost.localdomain>
2022-10-11 21:02:15 -0500

    feat: CRUD posts

--- main.go
+++ main.go
@@ -15,9 +15,8 @@ import (
 
 func main() {
 	err := godotenv.Load(".env")
-
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file %v\n", err)
 	}
 
 	PORT := os.Getenv("PORT")
@@ -25,24 +24,32 @@ func main() {
 	DATABASE_URL := os.Getenv("DATABASE_URL")
 
 	s, err := server.NewServer(context.Background(), &server.Config{
-		JWTSecret:   JWT_SECRET,
 		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
 		DatabaseUrl: DATABASE_URL,
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error creating server %v\n", err)
 	}
 
 	s.Start(BindRoutes)
 }
 
 func BindRoutes(s server.Server, r *mux.Router) {
-	r.Use(middleware.CheckAuthMiddleware(s))
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+
+	api.Use(middleware.CheckAuthMiddleware(s))
 
 	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
-	r.HandleFunc("/signup", handlers.SignUpHandler(s)).Methods(http.MethodPost)
-	r.HandleFunc("/login", handlers.LoginHandler(s)).Methods(http.MethodPost)
-	r.HandleFunc("/me", handlers.MeHandler(s)).Methods(http.MethodGet)
-	r.HandleFunc("/posts", handlers.InsertPostHandler(s)).Methods(http.MethodPost)
+
+	r.HandleFunc("/api/v1/signup", handlers.SignUpHandler(s)).Methods(http.MethodPost)
+	r.HandleFunc("/api/v1/login", handlers.LoginHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/me", handlers.MeHandler(s)).Methods(http.MethodGet)
+
+	api.HandleFunc("/posts", handlers.InsertPostHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/posts/{postId}", handlers.DeletePostByIdHandler(s)).Methods(http.MethodDelete)
+	api.HandleFunc("/posts/{postId}", handlers.UpdatePostByIdHandler(s)).Methods(http.MethodPut)
+	r.HandleFunc("/api/v1/posts/{postId}", handlers.GetPostByIDHandler(s)).Methods(http.MethodGet)
 }
